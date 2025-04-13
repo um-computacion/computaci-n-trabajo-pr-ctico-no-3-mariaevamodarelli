d58b0140bbe6b82392ac5dfa4eeefd32dce833e9
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2025-04-13 18:22:15 -0300

    Agregando una nueva excepción llamada SalirDelPrograma y tambien agregue sus tests

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -2,6 +2,7 @@ import unittest
 from src.exceptions import (
     ingrese_numero,
     NumeroDebeSerPositivo,
+    SalirDelPrograma
 )
 from unittest.mock import patch
 
@@ -82,6 +83,25 @@ class TestCalculoNumeros(unittest.TestCase):
     @patch('builtins.input', return_value='   ')
     def test_espacios_vacios(self, mock_input):
         with self.assertRaises(ValueError):
+            ingrese_numero()
+
+
+#tests para la excepción SalirDelPrograma
+
+
+    @patch('builtins.input', return_value='salir')
+    def test_salir_del_programa(self, mock_input):
+        with self.assertRaises(SalirDelPrograma):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='SALIR')
+    def test_salir_del_programa_mayúsculas(self, mock_input):
+        with self.assertRaises(SalirDelPrograma):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='Salir')
+    def test_salir_del_programa_con_mayúsculas(self, mock_input):
+        with self.assertRaises(SalirDelPrograma):
             ingrese_numero()
 
 if __name__ == '__main__':
